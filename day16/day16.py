1f3f22a3a81edfe7a85492342bc08d85f4c2ab12
Viktor Karlsson <viktor.karlsson.2041@gmail.com>
2022-12-22 13:07:37 +0100

    task1 complete

--- day16/day16.py
+++ day16/day16.py
@@ -10,56 +10,49 @@ def read_input(filename):
 
     return valves
 
-def get_total_flow_of_valve(flow, timer):
-    return flow*timer
+lines = [re.split('[\\s=;,]+', x) for x in open("input.txt").read().splitlines()]
+G = {x[1]: set(x[10:]) for x in lines}
+F = {x[1]: int(x[5]) for x in lines if int(x[5]) != 0}
+I = {x: 1<<i for i, x in enumerate(F)}
+T = {x: {y: 1 if y in G[x] else float('+inf') for y in G} for x in G}
+for k in T:
+    for i in T:
+        for j in T:
+            T[i][j] = min(T[i][j], T[i][k]+T[k][j])
 
-def get_optimal_flow_of_neighbour(neighbours, valves, total_flow, timer, visited_valves):
-    optimal_neighbour_flow = total_flow
+def find_max_flow(current_valve, state, timer, flow, answer):
+    answer[state] = max(answer.get(state, 0), flow)
 
-    for valve in neighbours:
-        # Loop through already not visisted neighbours
-        if valve not in visited_valves:
-            visited_valves.add(valve)
-            valve_flow = find_max_flow(valves, valve, total_flow, timer-1, visited_valves) # Recursive call
+    for valve in F:
+        # Subtract the number of steps to get from current_valve to valve,
+        new_timer = timer - T[current_valve][valve] - 1
 
-            # Update the optimal flow
-            optimal_neighbour_flow = max(optimal_neighbour_flow, valve_flow)
+        # Stop criterium -> All valves visited or timer has run out 
+        if I[valve] & state or new_timer <= 0:
+            continue
+        
+        # Calculate the new flow
+        new_flow = flow + F[valve] * new_timer
 
-    return optimal_neighbour_flow
+        # Recursive call
+        find_max_flow(valve, state | I[valve], new_timer, new_flow, answer)
 
-def find_max_flow(valves, current_valve, total_flow, timer, visited_valves):
-    optimal_flow = total_flow
-
-    # Stop criterium
-    if timer == 0:
-        return optimal_flow
-    
-    # Get the flow of the current valve and its neighbours
-    flow, neighbours = valves[current_valve]
-
-    # We have 2 choices:
-    # 1. We open the valve and if we have time, go to another valve
-    valve_flow = get_total_flow_of_valve(flow, timer-1)
-    choice1 = get_optimal_flow_of_neighbour(neighbours, valves, total_flow+valve_flow, timer-2, visited_valves)
-
-    # 2. We go to another valve without opening the current valve
-    choice2 = get_optimal_flow_of_neighbour(neighbours, valves, total_flow, timer-1, visited_valves)
-
-    optimal_flow = max(choice1, choice2)
-
-    return optimal_flow
+    return answer
 
 
 def solve_task1():
-    valves = read_input("input.txt")
     timer = 30
     start_valve = "AA"
-    visited_valves = set([start_valve])
 
-    maximum_flow = find_max_flow(valves, start_valve, 0, timer, visited_valves)
+    maximum_flow = max(find_max_flow(start_valve, 0, timer, 0, {}).values())
 
     print("Solution Task 1: Maximum flow =", maximum_flow)
 
 
-
 solve_task1()
+
+
+#print(G) # graph
+#print(F) # flows where flow > 0
+#print(I) # Each valve with flow > 0 connected to a value
+#print(T) # Transition graph? Number of steps to each node from anywhere in the graph.
