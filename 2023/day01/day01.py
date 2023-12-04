1b3af09201832c727e78aa0e541e14a7640fb40f
Viktor Karlsson <viktor.karlsson.2041@gmail.com>
2023-12-04 20:51:48 +0100

    finish day 1

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -1,46 +1,81 @@
+from time import time
 
 digits = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine' : 9
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine' : '9'
 }
 
-def findFirstNumberPart1(line):
+def timer_func(func):
+    # This function shows the execution time of
+    # the function object passed
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f"Function {func.__name__!r} executed in {(t2 - t1):.4f}s")
+        return result
+
+    return wrap_func
+
+def findFirstNumber(line, reverse=False, part1=True):
+    line = line[::-1] if reverse else line
+
+    if part1:
+        return findFirstNumberP1(line)
+    else:
+        return findFirstNumberP2(line, reverse)
+
+
+def findFirstNumberP1(line):
     for c in line:
         if c.isnumeric():
             return c
         
     return None
 
-def findFirstNumberPart2(line):
+
+def findFirstNumberP2(line, reversed):
+    n = len(line)
     for index, c in enumerate(line):
-        
         if c.isnumeric() :
             return c
         
-        for digit, value in digits:
-            if (index + len(digit))
-            s = line[index, index+len(digit)]
-        
+        for number in digits:
+            if (index + len(number) < n):
+                s = line[index : index + len(number)]
+                s = s[::-1] if reversed else s
+
+                if s == number:
+                    return digits[number]
+
     return None
 
+@timer_func
+def main(part1=True):
+    with open("input.txt") as f:
+        sum = 0
+        for line in f.readlines():
+            
+            number1 = findFirstNumber(line=line, reverse=False, part1=part1)
+            number2 = findFirstNumber(line=line, reverse=True, part1=part1)
 
-
-with open("input.txt") as f:
-    sum = 0
-    for line in f.readlines():
+            number = number1 + number2 
+            sum += int(number)
         
-        number1 = findFirstNumberPart1(line)
-        number2 = findFirstNumberPart1(reversed(line))
+        return sum
 
-        number = number1 + number2 
-        sum += int(number)
-    
-    print("Answer: ", sum)
+
+if __name__ == "__main__":
+    print("Answer Task 1: {}".format(main(True)))
+    print("Answer Task 2: {}".format(main(False)))
+
+
+
 
