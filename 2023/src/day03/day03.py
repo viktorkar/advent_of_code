5f172332e63bad5af4d5d330673a8fd00aee40ca
Viktor Karlsson <viktor.karlsson.2041@gmail.com>
2023-12-07 16:32:07 +0100

    implement day3 p2

--- 2023/src/day03/day03.py
+++ 2023/src/day03/day03.py
@@ -1,5 +1,6 @@
 from time import time
 
+
 def timer_func(func):
     # This function shows the execution time of
     # the function object passed
@@ -12,14 +13,16 @@ def timer_func(func):
 
     return wrap_func
 
+
 def get_data(file_path="input.txt"):
     symbol_positions = set()
     numbers = []
+    gears = {}
 
     with open(file_path) as f:
         for row, line in enumerate(f):
             line = line.strip()
-            number, i, j = '', -1, -1
+            number, i, j = "", -1, -1
 
             for col, char in enumerate(line):
                 if char.isdigit():
@@ -29,49 +32,54 @@ def get_data(file_path="input.txt"):
                 else:
                     if number:
                         numbers.append((number, i, j))
-                        number, i, j = '', -1, -1
+                        number, i, j = "", -1, -1
 
-                    if char != '.':
+                    if char != ".":
                         symbol_positions.add((row, col))
-            
+
+                    if char == "*":
+                        gears[(row, col)] = []
+
             if number:
                 numbers.append((number, i, j))
 
-    return numbers, symbol_positions
+    return numbers, symbol_positions, gears
+
 
 def get_neighboring_points(number_data):
-    neighbors = [
-        (-1, -1), (-1, 0), (-1, 1),
-        (0, -1),           (0, 1),
-        (1, -1), (1, 0), (1, 1)
-    ]
+    neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
     number, row, col_start = number_data
     col_end = col_start + len(number)
-    word_cols = list(range(col_start, col_end))
+    word_cols = range(col_start, col_end)
 
-    return set((row + dr, col + dc) for col in word_cols for dr, dc in neighbors)
+    return {(row + dr, col + dc) for col in word_cols for dr, dc in neighbors}
+
 
 @timer_func
-def part1():
+def solve():
     sum = 0
-    numbers, symbol_positions = get_data()
+    gear_ratios = 0
+    numbers, symbol_positions, gears = get_data()
 
     for number_data in numbers:
         neighboring_points = get_neighboring_points(number_data)
-        
+
         if any(point in symbol_positions for point in neighboring_points):
             sum += int(number_data[0])
-    
-    return sum
+
+        for point in neighboring_points:
+            if point in gears:
+                gears[point].append(int(number_data[0]))
+
+    for connected_numbers in gears.values():
+        if len(connected_numbers) == 2:
+            gear_ratios += connected_numbers[0] * connected_numbers[1]
+
+    return sum, gear_ratios
+
 
 if __name__ == "__main__":
-    print("Answer Task 1: {}".format(part1()))
-   # print("Answer Task 2: {}".format(part2()))
-        
-
-
-
-
-                    
-                    
+    p1, p2 = solve()
+    print("Answer Task 1: {}".format(p1))
+    print("Answer Task 2: {}".format(p2))
